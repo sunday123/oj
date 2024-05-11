3adf5b2d8e6afc1e8245de44970c002327d0cc74
mongo <linyongxinn@gmail.com>
2024-05-11 10:57:03 +0800

    oj

---
+++ src/com/ij34/oj/leetcode/FindDuplicate.java
@@ -0,0 +1,35 @@
+package com.ij34.oj.leetcode;
+
+/**
+ * 287. 寻找重复数
+ * 其数字都在 [1, n] 范围内（包括 1 和 n），可知至少存在一个重复的整数
+ * @Author: ij34
+ * @Date: 2024-05-11
+ */
+
+public class FindDuplicate {
+    public static void main(String[] args) {
+        int[] a = new int[]{4,3,1,4,2};
+        System.out.println(findDuplicate(a));
+    }
+    public static int findDuplicate(int[] nums) {
+          int slow =0, fast =0;
+          while (true){
+              fast=nums[nums[fast]];
+              slow = nums[slow];
+              if (fast==slow){
+                  break;
+              }
+          }
+          slow=0;
+          while (true){
+            slow=nums[slow];
+            fast=nums[fast];
+            if (fast==slow){
+                break;
+            }
+          }
+          return slow;
+
+    }
+}
