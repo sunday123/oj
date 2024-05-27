816c056e971f88c71d6410ec81aaf39d0e3e800a
mongo <linyongxinn@gmail.com>
2024-05-27 19:45:45 +0800

    员工派遣

--- src/com/ij34/oj/huawei/EmployeeAssignment.java
+++ src/com/ij34/oj/huawei/EmployeeAssignment.java
@@ -2,6 +2,8 @@ package com.ij34.oj.huawei;
 
 /**
  * 员工派遣
+ * https://hydro.ac/d/HWOD2023/p/OD395
+ *
  * @Author: ij34
  * @Date: 2024-05-08
  */
@@ -12,18 +14,44 @@ public class EmployeeAssignment {
         Scanner scanner = new Scanner(System.in);
         int x = scanner.nextInt(); // 国家x的代号
         int y = scanner.nextInt(); // 国家y的代号
-        long cntx = scanner.nextLong(); // 国家x需要的员工数
-        long cnty = scanner.nextLong(); // 国家y需要的员工数
-        int k=1;
-        while (cntx>0 || cnty>0){
-            if (cntx>0 && k%x!=0){
-                cntx--;
-            }else if (cnty>0 && k%y!=0){
-                cnty--;
-            }
-            k++;
+        int cntx = scanner.nextInt(); // 国家x需要的员工数
+        int cnty = scanner.nextInt(); // 国家y需要的员工数
+        int left=1,right=1000_000_000;
+        while (left<=right){
+             int mid = (left+right)/2;
+             if (isOk(mid,x,y,cntx,cnty)){
+                 right=mid-1;
+             }else {
+                 left=mid+1;
+             }
         }
-        System.out.println(k-1);
+        System.out.println(right+1);
     }
+
+    private static boolean isOk(int total, int x, int y, int cntx, int cnty) {
+        int notX = total/x;
+        int notY = total/y;
+        int notXNotY = total/(x*y/gcm(x,y));
+        int availableX = total-notX;
+        int availableY = total-notY;
+        int common = total-(notX+notY-notXNotY);
+        if (availableX<cntx || availableY<cnty){
+            return false;
+        }
+        if ((availableX+availableY-common)>=cntx+cnty){
+            return true;
+        }
+        return false;
+
+    }
+
+
+    private static int gcm(int x, int y) {
+        if (x==0){
+            return y;
+        }
+        return gcm(y%x,x);
+    }
+
 
 }
