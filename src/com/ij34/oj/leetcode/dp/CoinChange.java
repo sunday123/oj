d0234f7c84ce0d0e66faa2a4d68e56ebd2dc7d74
mongo <linyongxinn@gmail.com>
2024-05-28 10:08:16 +0800

    零钱兑换

--- src/com/ij34/oj/leetcode/dp/CoinChange.java
+++ src/com/ij34/oj/leetcode/dp/CoinChange.java
@@ -47,4 +47,25 @@ public class CoinChange {
         }
         return dp[coins.length][amount];
     }
+
+
+    public int coinChange3(int[] coins, int amount) {
+        int[][] dp = new int[coins.length+1][amount+1];//dp[i][j] 表示前 i 种硬币凑成总额 j 所需的最少硬币数
+        for (int i=0;i<dp.length;i++){
+            Arrays.fill(dp[i],amount+1);
+        }
+        for (int i=0;i<coins.length+1;i++){//金额为0,不需要任何硬币
+            dp[i][0]=0;
+        }
+        for (int i=1;i<=coins.length;i++){
+            for (int j=1;j<=amount;j++){
+                if (j>=coins[i-1]){
+                    dp[i][j]=Math.min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
+                }else {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount]==amount+1?-1:dp[coins.length][amount];
+    }
 }
